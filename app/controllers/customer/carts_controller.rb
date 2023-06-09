66f92b581f5f62f86b77851881dd3be214319f48
sanjanapatelSP <iampatel.sanjana@gmail.com>
2023-06-09 10:19:19 +0530

    add category and subcategories

--- app/controllers/customer/carts_controller.rb
+++ app/controllers/customer/carts_controller.rb
@@ -1,15 +1,12 @@
+
 class Customer::CartsController < ApplicationController
   def index
     @cart_items = current_user.carts.where(isorder:false).first.cart_items
   end
-#hhhh
+
   def new
-  # Add Product to cart
-  # hello
   @cart_item = current_user.carts.where(isorder:false).first.cart_items.where(product_id:params[:product_id])
-  debugger
   if @cart_item.empty?
-    debugger
       @cart_item = current_user.carts.first.cart_items.new(price: params[:price], quantity: 1,product_id:params[:product_id])
       @cart_item.save
   else
--- app/controllers/supplier/products_controller.rb
+++ app/controllers/supplier/products_controller.rb
@@ -1,41 +1,46 @@
 class Supplier::ProductsController < ApplicationController
-        def index
-            @products = Product.all
-        end
-    
-        def show
-            @product = Product.find(params[:id])
-        end
-    
-        def new
-            @product = Product.new
-        end
-    
-        def create
-            @product = Product.new(product_params)    
-            @product[:category_id]=2
-            if @product.save
-              redirect_to [:supplier,@product]
-            else
-              render :show, status: :unprocessable_entity
-            end
-        end
+  def index
+      @products = Product.all
+  end
 
-        def edit
-            @product = Product.find(params[:id])
-        end
+  def show
+      @product = Product.find(params[:id])
+  end
 
-        def update
-            @product = Product.find(params[:id])       
-            if Product.update(product_params)
-              redirect_to [:supplier,@product]
-            else
-              render :edit, status: :unprocessable_entity
-            end
-        end
-    
-        private
-        def product_params
-          params.require(:product).permit(:name, :current_price, :actual_price, :image)
-        end
+  def new
+      @product = Product.new
+  end
+
+  def create
+      @product = Product.new(product_params)    
+      @product[:category_id]=params[:category_id]
+      if @product.save
+        redirect_to [:supplier,@product]
+      else
+        render :show, status: :unprocessable_entity
+      end
+  end
+
+  def edit
+      @product = Product.find(params[:id])
+  end
+
+  def update
+      @product = Product.find(params[:id])       
+      if Product.update(product_params)
+        redirect_to [:supplier,@product]
+      else
+        render :edit, status: :unprocessable_entity
+      end
+  end
+
+  def addsubcategories
+    @subcategories = Category.find(params[:id]).subcategories
+    render json: @subcategories
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :current_price, :actual_price, :image)
+  end
 end
